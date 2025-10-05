0abcf18f2eff69f0b1e17b135ae2e6060c4ca01a
yooper <dcardin2007@gmail.com>
2025-10-05 14:31:18 -0400

    can collect single images, added for playwright test

---
+++ tests/pages/popup.spec.ts
@@ -0,0 +1,14 @@
+/**
+ * Test content script
+ */
+import { test, expect } from '../fixtures';
+
+//test('example test', async ({ page }) => {
+//  await page.goto('https://example.com');
+//  await expect(page.locator('body')).toHaveText('Changed by my-extension');
+//});
+
+test('popup page', async ({ page, extensionId }) => {
+  await page.goto(`chrome-extension://${extensionId}/popup.html`);
+  await expect(page.getByText('NEVER')).toBeVisible()
+});
