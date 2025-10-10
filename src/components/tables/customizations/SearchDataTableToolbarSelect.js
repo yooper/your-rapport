08479129916d6e010c20fcdd85f78e4ecc8bff86
yooper <yooper@users.noreply.github.com>
2025-10-10 07:12:43 -0400

    image merging works (#22)

--- src/components/tables/customizations/SearchDataTableToolbarSelect.js
+++ src/components/tables/customizations/SearchDataTableToolbarSelect.js
@@ -4,9 +4,12 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import { withStyles } from 'tss-react/mui';
 import Tooltip from "@mui/material/Tooltip";
 import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
-import { downloadJsonData } from '../../../utilities/transformers';
+import { downloadBase64Image, downloadJsonData } from '../../../utilities/transformers';
 import { RAPPORT } from '../../../services/constants';
 import { getLocalItem } from '../../../models/db/local';
+import BurstModeIcon from '@mui/icons-material/BurstMode';
+import { mergeImagesVertically } from '../../../services/image_loading_services';
+import { hideLoader, showLoader } from '../../../utilities/loaders';
 
 const defaultToolbarSelectStyles = theme => ({
   root: {
@@ -33,7 +36,7 @@ const defaultToolbarSelectStyles = theme => ({
 });
 
 function SearchDataTableToolbarSelect(props) {
-  const { classes, selectedRows, displayData, columns } = props;
+  const { classes, selectedRows, displayData, columns, rows } = props;
   /**
    * @param {number[]} selectedRows Array of rows indexes that are selected, e.g. [0, 2] will select first and third rows in table
    */
@@ -56,6 +59,22 @@ function SearchDataTableToolbarSelect(props) {
   return (
       <Fragment>
         <span style={{justifyContent: 'right'}}>
+          <Tooltip title={'Merge multiple screenshots and download the image.'}>
+            <IconButton >
+              <BurstModeIcon onClick={async() => {
+                showLoader()
+                const orderedIndex = selectedRows.data.map(sr => sr.dataIndex);
+                const orderedRapports = [];
+                for(let idx of orderedIndex){
+                  orderedRapports.push(rows[idx])
+                }
+                const dataUri = await mergeImagesVertically(orderedRapports);
+                await downloadBase64Image(dataUri, 'your_rapport_merged_images.png');
+                hideLoader()
+              }}/>
+            </IconButton>
+
+          </Tooltip>
           <Tooltip title={"Delete"}>
             <IconButton >
               <DeleteIcon onClick={() => {props.onRowsDelete(selectedRows, displayData)}}/>
@@ -64,19 +83,7 @@ function SearchDataTableToolbarSelect(props) {
           <Tooltip title={"Export Selected Rapports in a JSON file to share or backup."}>
             <IconButton>
               <CloudDownloadIcon onClick={async() => {
-                const indices = Object.keys(selectedRows.lookup);
-                const rows = displayData.filter(d => indices.includes(""+d.dataIndex));
-                let uuids = []
-                for(let i = 0; i < rows.length; i++)
-                {
-                  let record = {}
-                  for(let idx=0; idx < columns.length; idx++)
-                  {
-                      record[columns[idx].name] = rows[i].data[idx]
-                  }
-                  uuids.push(record.uuid)
-                }
-                const rapports = (await getLocalItem(RAPPORT) ?? []).filter(r => uuids.includes(r.uuid))
+                const rapports = getRapportRecords(selectedRows, displayData, columns);
                 downloadJsonData(rapports, 'your-rapports.json');
               }} />
             </IconButton>
@@ -84,6 +91,24 @@ function SearchDataTableToolbarSelect(props) {
         </span>
       </Fragment>
   )
+}
+
+async function getRapportRecords(selectedRows, displayData, columns)
+{
+  const indices = Object.keys(selectedRows.lookup);
+  const rows = displayData.filter(d => indices.includes(""+d.dataIndex));
+  let uuids = []
+  for(let i = 0; i < rows.length; i++)
+  {
+    let record = {}
+    for(let idx=0; idx < columns.length; idx++)
+    {
+        record[columns[idx].name] = rows[i].data[idx]
+    }
+    uuids.push(record.uuid)
+  }
+  const rapports = (await getLocalItem(RAPPORT) ?? []).filter(r => uuids.includes(r.uuid))
+  return rapports
 }
 
 export default withStyles(SearchDataTableToolbarSelect, defaultToolbarSelectStyles, { name: 'SearchDataTableToolbarSelect' });
